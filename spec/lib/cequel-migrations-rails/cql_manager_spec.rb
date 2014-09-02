6dcd83bcfa6240655b78034b6978754125f52a5e
Christopher Evans <christopher.evans@reachlocal.com>
2014-09-02 15:40:19 -0700

    Test upgrading the specs

--- spec/lib/cequel-migrations-rails/cql_manager_spec.rb
+++ spec/lib/cequel-migrations-rails/cql_manager_spec.rb
@@ -9,17 +9,17 @@ require 'spec_helper'
 describe Cequel::Migrations::Rails::KeyspaceManager do
   describe "#new" do
     it "creates a CQL connection for a single host" do
-      Cequel::Migrations::Rails::KeyspaceManager.stub(:cequel_env_conf).and_return({ 'host' => 'some host' })
+      Cequel::Migrations::Rails::KeyspaceManager.double(:cequel_env_conf).and_return({ 'host' => 'some host' })
       CassandraCQL::Database.should_receive(:new).with('some host')
       Cequel::Migrations::Rails::KeyspaceManager.new
     end
     it "creates a CQL connection for an array of hosts" do
-      Cequel::Migrations::Rails::KeyspaceManager.stub(:cequel_env_conf).and_return({ 'hosts' => ['some host', 'another host'] })
+      Cequel::Migrations::Rails::KeyspaceManager.double(:cequel_env_conf).and_return({ 'hosts' => ['some host', 'another host'] })
       CassandraCQL::Database.should_receive(:new).with(['some host', 'another host'])
       Cequel::Migrations::Rails::KeyspaceManager.new
     end
     it "creates a CQL connection for an array of hosts when the port is specified" do
-      Cequel::Migrations::Rails::KeyspaceManager.stub(:cequel_env_conf).and_return({ 'hosts' => ['host1', 'host2'], 'port' => '9042'})
+      Cequel::Migrations::Rails::KeyspaceManager.double(:cequel_env_conf).and_return({ 'hosts' => ['host1', 'host2'], 'port' => '9042'})
       Cql::Client.should_receive(:connect).with(['host1', 'host2'])
       Cequel::Migrations::Rails::KeyspaceManager.new
     end
@@ -27,10 +27,10 @@ describe Cequel::Migrations::Rails::KeyspaceManager do
 
   describe "#create_keyspace" do
     it "send cql to create the specified keyspace" do
-      Cequel::Migrations::Rails::KeyspaceManager.stub(:cequel_env_conf).and_return({ 'host' => 'some host', 'keyspace' => 'aoeuoae', 'strategy_class' => 'stratclass' })
+      Cequel::Migrations::Rails::KeyspaceManager.double(:cequel_env_conf).and_return({ 'host' => 'some host', 'keyspace' => 'aoeuoae', 'strategy_class' => 'stratclass' })
       db = mock('db')
-      CassandraCQL::Database.stub(:new).and_return(db)
-      subject.stub(:build_create_keyspace_cmd).and_return('xxx')
+      CassandraCQL::Database.double(:new).and_return(db)
+      subject.double(:build_create_keyspace_cmd).and_return('xxx')
       db.should_receive(:execute).with('xxx')
       subject.create_keyspace
     end
@@ -38,23 +38,23 @@ describe Cequel::Migrations::Rails::KeyspaceManager do
 
   describe "#build_create_keyspace_cmd" do
     it "build the cql command string to create a keyspace" do
-      Cequel::Migrations::Rails::KeyspaceManager.stub(:cequel_env_conf).and_return({ 'host' => 'some host' })
-      CassandraCQL::Database.stub(:new)
+      Cequel::Migrations::Rails::KeyspaceManager.double(:cequel_env_conf).and_return({ 'host' => 'some host' })
+      CassandraCQL::Database.double(:new)
       subject.send(:build_create_keyspace_cmd, 'somename_keyspace_name', 'StrategyClass', { :replication_factor => 1 }).should == "CREATE KEYSPACE somename_keyspace_name WITH strategy_class = 'StrategyClass' AND strategy_options:replication_factor = 1"
     end
 
     it "builds the cql command string to create a keyspace without strategy_options" do
-      Cequel::Migrations::Rails::KeyspaceManager.stub(:cequel_env_conf).and_return({ 'host' => 'some host' })
-      CassandraCQL::Database.stub(:new)
+      Cequel::Migrations::Rails::KeyspaceManager.double(:cequel_env_conf).and_return({ 'host' => 'some host' })
+      CassandraCQL::Database.double(:new)
       subject.send(:build_create_keyspace_cmd, 'somename_keyspace_name', 'StrategyClass', nil).should == "CREATE KEYSPACE somename_keyspace_name WITH strategy_class = 'StrategyClass'"
     end
   end
 
   describe "#use_keyspace" do
     it "send the cql command to use the keyspace from the cequel.yml" do
-      Cequel::Migrations::Rails::KeyspaceManager.stub(:cequel_env_conf).and_return({ 'host' => 'some host', 'keyspace' => 'my_keyspace' })
+      Cequel::Migrations::Rails::KeyspaceManager.double(:cequel_env_conf).and_return({ 'host' => 'some host', 'keyspace' => 'my_keyspace' })
       db = mock('db')
-      CassandraCQL::Database.stub(:new).and_return(db)
+      CassandraCQL::Database.double(:new).and_return(db)
       db.should_receive(:execute).with('USE my_keyspace')
       subject.use_keyspace
     end
@@ -62,9 +62,9 @@ describe Cequel::Migrations::Rails::KeyspaceManager do
 
   describe "#drop_keyspace" do
     it "sends the cql comand to drop the keyspace from the cequel.yml" do
-      Cequel::Migrations::Rails::KeyspaceManager.stub(:cequel_env_conf).and_return({ 'host' => 'some host', 'keyspace' => 'my_keyspace' })
+      Cequel::Migrations::Rails::KeyspaceManager.double(:cequel_env_conf).and_return({ 'host' => 'some host', 'keyspace' => 'my_keyspace' })
       db = mock('db')
-      CassandraCQL::Database.stub(:new).and_return(db)
+      CassandraCQL::Database.double(:new).and_return(db)
       db.should_receive(:execute).with('DROP KEYSPACE my_keyspace')
       subject.drop_keyspace
     end
--- spec/lib/cequel/migration_spec.rb
+++ spec/lib/cequel/migration_spec.rb
@@ -7,12 +7,12 @@ describe Cequel::Migration do
 
   describe "#new" do
     it "create a cassandra-cql database connection for the host & keyspace specified in the environment's config" do
-      migration_class.stub(:cequel_env_conf).and_return({ 'host' => 'somehost', 'keyspace' => 'somekeyspace' })
+      migration_class.double(:cequel_env_conf).and_return({ 'host' => 'somehost', 'keyspace' => 'somekeyspace' })
       CassandraCQL::Database.should_receive(:new).with('somehost', { :keyspace => 'somekeyspace' }, {})
       migration
     end
     it "create a cql-rb database connection for the host & keyspace specified in the environment's config if the port is 9042" do
-      migration_class.stub(:cequel_env_conf).and_return({ 'host' => 'somehost', 'keyspace' => 'somekeyspace', 'port' => '9042' })
+      migration_class.double(:cequel_env_conf).and_return({ 'host' => 'somehost', 'keyspace' => 'somekeyspace', 'port' => '9042' })
       Cql::Client.should_receive(:connect).with('somehost', { :keyspace => 'somekeyspace' })
       migration
     end
@@ -20,17 +20,17 @@ describe Cequel::Migration do
   
   describe "#execute" do
     it "delegates to the cassandra-cql connection execute" do
-      migration_class.stub(:cequel_env_conf).and_return({ 'keyspace' => 'test keyspace', 'host' => '123.123.123.123' })
-      db = mock('db').as_null_object
-      CassandraCQL::Database.stub(:new).and_return(db)
+      migration_class.double(:cequel_env_conf).and_return({ 'keyspace' => 'test keyspace', 'host' => '123.123.123.123' })
+      db = double('db').as_null_object
+      CassandraCQL::Database.double(:new).and_return(db)
       db.should_receive(:execute).with("some cql string")
       migration.execute("some cql string")
     end
 
     it "delegates to the cql-rb connection execute" do
-      migration_class.stub(:cequel_env_conf).and_return({ 'keyspace' => 'test keyspace', 'host' => '123.123.123.123','port' => '9042' })
-      db = mock('db').as_null_object
-      Cql::Client.stub(:connect).and_return(db)
+      migration_class.double(:cequel_env_conf).and_return({ 'keyspace' => 'test keyspace', 'host' => '123.123.123.123','port' => '9042' })
+      db = double('db').as_null_object
+      Cql::Client.double(:connect).and_return(db)
       db.should_receive(:execute).with("some cql string")
       migration.execute("some cql string")
     end
@@ -38,16 +38,16 @@ describe Cequel::Migration do
 
   describe ".cequel_conf_path" do
     it "returns the path to the cequel conf file" do
-      ::Rails.stub(:root).and_return('/foo/bar')
+      ::Rails.double(:root).and_return('/foo/bar')
       migration_class.cequel_conf_path.should eq('/foo/bar/config/cequel.yml')
     end
   end
 
   describe ".cequel_conf_file" do
     it "returns the file object of the file at the cequel_conf_path" do
-      conf_path = stub
-      conf_file = stub
-      migration_class.stub(:cequel_conf_path).and_return(conf_path)
+      conf_path = double
+      conf_file = double
+      migration_class.double(:cequel_conf_path).and_return(conf_path)
       File.should_receive(:open).with(conf_path).and_return(conf_file)
       migration_class.cequel_conf_file.should eq(conf_file)
     end
@@ -55,9 +55,9 @@ describe Cequel::Migration do
 
   describe ".cequel_conf" do
     it "returns the hash result of YAML loading the cequel.yml conf file" do
-      conf_file = stub
-      conf_hash = stub
-      migration_class.stub(:cequel_conf_file).and_return(conf_file)
+      conf_file = double
+      conf_hash = double
+      migration_class.double(:cequel_conf_file).and_return(conf_file)
       ::YAML.should_receive(:load).with(conf_file).and_return(conf_hash)
       migration_class.cequel_conf.should eq(conf_hash)
     end
@@ -70,17 +70,17 @@ describe Cequel::Migration do
     end
 
     it "grabs the Rails.env section of the config" do
-      conf = mock
-      ::Rails.stub(:env).and_return('blue')
+      conf = double
+      ::Rails.double(:env).and_return('blue')
       conf.should_receive(:[]).with('blue')
-      migration_class.stub(:cequel_conf).and_return(conf)
+      migration_class.double(:cequel_conf).and_return(conf)
       migration_class.cequel_env_conf
     end
 
     context "when the environment is found in the config" do
       before do
-        ::Rails.stub(:root).and_return(File.expand_path('../../../fixtures', __FILE__))
-        ::Rails.stub(:env).and_return('development')
+        ::Rails.double(:root).and_return(File.expand_path('../../../fixtures', __FILE__))
+        ::Rails.double(:env).and_return('development')
       end
 
       it "returns that environments portion of the config as a hash" do
@@ -101,8 +101,8 @@ describe Cequel::Migration do
 
     context "when the environment is NOT found in the config" do
       before do
-        ::Rails.stub(:root).and_return(File.expand_path('../../../fixtures', __FILE__))
-        ::Rails.stub(:env).and_return('nonexistentenvironment')
+        ::Rails.double(:root).and_return(File.expand_path('../../../fixtures', __FILE__))
+        ::Rails.double(:env).and_return('nonexistentenvironment')
       end
 
       it "returns nil" do
@@ -114,24 +114,24 @@ describe Cequel::Migration do
   describe "#thrift_options" do
     context "when the environment is found in the config" do
       before do
-        ::Rails.stub(:root).and_return(File.expand_path('../../../fixtures', __FILE__))
-        ::Rails.stub(:env).and_return('development')
+        ::Rails.double(:root).and_return(File.expand_path('../../../fixtures', __FILE__))
+        ::Rails.double(:env).and_return('development')
       end
 
       context "when thrift options are found in the config" do
         it "returns the thrift options as a hash" do
-          CassandraCQL::Database.stub(:new).and_return(stub.as_null_object)
+          CassandraCQL::Database.double(:new).and_return(double.as_null_object)
           migration.send(:thrift_options).should eq({:connect_timeout=>5, :timeout=>10})
         end
       end
 
       context "when thrift options are NOT found in the config" do
         before do
-          ::Rails.stub(:env).and_return('test')
+          ::Rails.double(:env).and_return('test')
         end
 
         it "returns an empty hash" do
-          CassandraCQL::Database.stub(:new).and_return(stub.as_null_object)
+          CassandraCQL::Database.double(:new).and_return(double.as_null_object)
           migration.send(:thrift_options).should eq({})
         end
       end
